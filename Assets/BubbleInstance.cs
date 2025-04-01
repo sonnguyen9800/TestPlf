1820da25f84648af52a2634aa4d12f8e751d3a17
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2025-04-01 09:48:41 +0700

    bubble item added

---
+++ Assets/BubbleInstance.cs
@@ -0,0 +1,39 @@
+using Platformer.Gameplay;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// This class contains the data required for implementing token collection mechanics.
+    /// It does not perform animation of the token, this is handled in a batch by the 
+    /// TokenController in the scene.
+    /// </summary>
+    [RequireComponent(typeof(Collider2D))]
+    public class BubbleInstance : MonoBehaviour
+    {
+        internal bool collected = false;
+        internal SpriteRenderer _renderer;
+
+        void Awake()
+        {
+            _renderer = GetComponent<SpriteRenderer>();
+        }
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            //only exectue OnPlayerEnter if the player collides with this token.
+            var player = other.gameObject.GetComponent<PlayerController>();
+            if (player != null) OnPlayerEnter(player);
+        }
+
+        void OnPlayerEnter(PlayerController player)
+        {
+            Debug.LogError("Collect ballone");
+            if (collected) return;
+            collected = true;
+            _renderer.enabled = false;
+            var ev = Schedule<PlayerBalloneCollision>();
+        }
+    }
+}
